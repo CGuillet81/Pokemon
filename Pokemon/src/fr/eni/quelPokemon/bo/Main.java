73e43732511be24c0ddc1696a3d4287bf78d004d
cg068 <cg0681571909@gmail.com>
2021-06-04 11:10:50 +0200

    Commit 1

--- Pokemon/src/fr/eni/quelPokemon/bo/Main.java
+++ Pokemon/src/fr/eni/quelPokemon/bo/Main.java
@@ -1,2 +1,30 @@
-package fr.eni.quelPokemon.bo;public class Main {
+package fr.eni.quelPokemon.bo;
+
+public class Main {
+    public static void main(String[] args) {
+    Dresseur sacha = new Dresseur("Sacha", null);
+        Dresseur ondine = new Dresseur("Ondine", null);
+    Attaque statik = new Attaque("Statik",20);
+        Attaque paratonnerre = new Attaque("Paratonnerre",75);
+    Pokemon pikachu = new Pokemon("Pikachu", 6, (int) 0.4, 120,statik, paratonnerre, null);
+        Attaque cran = new Attaque("Cran",20);
+        Attaque agitation = new Attaque("Agitation",75);
+    Pokemon rattata = new Pokemon("Rattata", 3.5, 0.3, 120, cran, agitation, null);
+
+    sacha.capture(pikachu);
+    sacha.afficher();
+    pikachu.afficher();
+
+    ondine.capture(rattata);
+    ondine.afficher();
+    rattata.afficher();
+
+    pikachu.combat(rattata);
+    }
+
+
+
+
+
+
 }
